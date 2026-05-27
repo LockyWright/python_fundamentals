2b05fb5174341963cef02bf32e7a9a780bc592a2
LockyWright <lockywright4@gmail.com>
2026-05-27 08:29:50 +0100

    Delete Python_Fundamentals directory

--- Python_Fundamentals/filter_products.py
+++
@@ -1,8 +0,0 @@
-with open("products.csv") as p:
-    lines = p.readlines()
-
-for items in lines[1:]:
-    row = items.strip()
-    row = row.split(",")
-    if row[2] == "electronics":
-        print(row[0], row[1])
